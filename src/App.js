2b6760d3d05f8b591b218581d50d39a2eeb5cdeb
Ogelo10 <augustineofoegbu1@gmail.com>
2022-01-16 21:31:55 +0100

    Reactjs Income tracker app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header';
+import IncomeForm from './components/IncomeForm';
+import IncomeList from './components/IncomeList';
+
+function App() {
+
+  const [income, setIncome] = useState([]);
+  const [totalIncome, setTotalIncome] = useState(0);
+
+  useEffect(() => {
+    let temp = 0;
+    for(let i = 0; i < income.length; i++) {
+      temp += parseInt(income[i].price);
+
+      setTotalIncome(temp);
+    }
+  }, [income]);
+
+  return (
+    <div className="App">
+      <Header totalIncome={totalIncome} />
+      <IncomeForm income={income} setIncome={setIncome} />
+      <IncomeList income={income} setIncome={setIncome} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/IncomeList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import IncomeItem from './IncomeItem'
+
+function IncomeList({income, setIncome}) {
+
+    const removeIncome = i => {
+        let temp = income.filter((v, index) => index != i);
+        setIncome(temp);
+    }
+
+    const sortByDate = (a, b) => {
+        return a.date - b.date
+    }
+
+    return (
+        <div className='income-list'>
+            {
+            income.sort(sortByDate).map((value, index) => {
+                <IncomeItem key={key} income={value} index={index} removeIncome={removeIncome} />
+            })
+        }
+        </div>
+    )
+}
+
+export default IncomeList
